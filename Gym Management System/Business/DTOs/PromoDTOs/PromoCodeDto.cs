8a412bd8cbd9b3b3fce47d3d5a7468987702cebd
Aya <aya.ahmed.swe@gmail.com>
2026-06-14 18:43:12 +0300

    Added Promo DTOs

---
+++ Gym Management System/Business/DTOs/PromoDTOs/PromoCodeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Gym_Management_System.Business.DTOs.PromoDTOs
+{
+    public class PromoCodeDto
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public decimal DiscountPercentage { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidUntil { get; set; }
+        public int MaxUses { get; set; }
+    }
+}
---
+++ Gym Management System/Business/DTOs/PromoDTOs/UpdatePromoCodeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Gym_Management_System.Business.DTOs.PromoDTOs
+{
+    public class UpdatePromoCodeDto
+    {
+        public string Code { get; set; } = string.Empty;
+        public decimal DiscountPercentage { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidUntil { get; set; }
+        public int MaxUses { get; set; }
+    }
+}
